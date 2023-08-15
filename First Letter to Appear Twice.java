91635d25eb06988da7ee0ea527d228d43d757c09
aolwyn <adambayley2@gmail.com>
2023-08-14 21:30:13 -0400

    Day 2

---
+++ First Letter to Appear Twice.java
@@ -0,0 +1,17 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        //we will use a hashmap to keep track of occurences.
+        HashSet<Character> map = new HashSet<>();
+        int i = 0;
+
+        while (i < s.length()) {
+            if (map.contains(s.charAt(i))) {
+                break;
+            } else {
+                map.add(s.charAt(i));
+            }
+            i++;
+        }
+        return s.charAt(i);
+    }
+}
