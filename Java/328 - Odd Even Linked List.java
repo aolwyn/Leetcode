4769b0901b55d65d9fb193736f717740ee08c249
Adam Bayley <adambayley2@gmail.com>
2023-12-18 17:25:19 -0500

    re-do odd even LL

--- Java/328 - Odd Even Linked List.java
+++ Java/328 - Odd Even Linked List.java
@@ -11,30 +11,28 @@
 class Solution {
     public ListNode oddEvenList(ListNode head) {
         /*Algorithm:
-        - We are required to put all 'odd' numbered nodes at start, even at the end.
-        - we need an algorithm using constant space and linear time (O(1), O(n) respectively)
-        - we will have a head for the positive and a head for the negative. build both then connect.
-        - loop condition based on even head (even is end of list).
-            --> the algorithm is as follows:
-                - set the next odd node to the node following the even node (even.next). ( think odd, even, odd recurring).
-                - set current odd node to next odd node.
-                - repeat for the even node (even.next = odd.next)
-                - set current even node to next even node.
-        - 1 base case, null head or no next node.
+        - we want to separate odd and even nodes. the last odd node should point to the first even node.
+        - we want O(1) space complexity and O(n) time complexity so must be done in 1 pass.
+        - 1 base case, head is null or next node is null return head
+        - general algorithm is as follows, set the curr odd and even nodes to head and head.next, set next node to 2 ahead (skip the even node to next odd node) and same for even 
+        - when even / even.next is null, set the last odd node's next node to be the even head. return head because head was never changed.
         */
-        if(head == null || head.next == null) return head;
+
+        if(head == null || head.next == null)
+            return head;
         
         ListNode odd = head;
         ListNode even = head.next;
         ListNode evenHead = even;
 
-        while(even != null && even.next != null){
-            odd.next = even.next;
-            odd = odd.next;
-            even.next = odd.next;
+        while(even!= null && even.next!=null){
+            odd.next = odd.next.next;
+            odd = odd.next; 
+            even.next = even.next.next;
             even = even.next;
         }
         odd.next = evenHead;
         return head;
+
     }
 }
