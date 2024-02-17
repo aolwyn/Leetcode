cc5b247fb96c5bae8966fde3b7b0eb3df59245bc
Adam Bayley <adambayley2@gmail.com>
2024-02-17 11:33:18 -0500

    1512 number of identical pairs

--- Java/1512 - Number of Good Pairs.java
+++ Java/1512 - Number of Good Pairs.java
@@ -1,6 +1,14 @@
 class Solution {
     public int numIdenticalPairs(int[] nums) {
+        /*Algorithm:
+        - we are looking for identical number of pairs.
+        - the brute force method would be to just run 2x for loop
+        - other method is to use a hashmap and just count occurrences;
+            -> the number of pairs that can be made for a given # of occurrences is: [n * (n-1)] / 2
+        */
+
         if(nums.length == 0) return 0;
+        
         HashMap<Integer,Integer> map = new HashMap<>();
         int pairs = 0;
 
