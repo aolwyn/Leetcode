8e27356831e9f517b5c253bb35b7cdffbda14fa2
Adam Bayley <adambayley2@gmail.com>
2024-03-05 22:29:35 -0500

    updated maximum number of coins you can get

--- Java/1561 - Maximum Number of Coins You Can Get.java
+++ Java/1561 - Maximum Number of Coins You Can Get.java
@@ -1,25 +1,27 @@
 class Solution {
     public int maxCoins(int[] piles) {
         /*Algorithm:
-        - general idea is to pick smallest # and then the 2 highest values in each iteration
-        - sort the array first
-        - we can 2 pointer. the left pointer increments by one but the right pointer goes by 2
-        - for the right pointer, we want to precrement add the value to sum then decrement again after before next iteration.
+        - we have 3n piles.
+        - in each step, alice takes the max. you pick the next max.
+        - bob takes the remainder.
+        - looking at this, we can understand that it makes sense to always give bob the lowest possible value and then take the remaining highest
+        available for you and alice.
+        - we can sort and use a 2 pointer technique to solve this.
         */
-        if(piles.length < 3)
+
+        if(piles.length ==0 || piles == null)
             return 0;
         
         Arrays.sort(piles);
-
+        int max = 0;
         int left = 0;
-        int right = piles.length-1;
-        int sum = 0;
+        int right = piles.length-2;
 
         while(left <= right){
             left++;
-            sum+= piles[--right];
-            right--;
+            max += piles[right];
+            right -=2;
         }
-        return sum;
+        return max;
     }
 }
