9fc158e3d6d7beb4f6b60959a14fb951bcf03b34
Adam Bayley <94658867+aolwyn@users.noreply.github.com>
2026-01-17 15:31:41 -0500

    Add solution for 'Contains Duplicate' problem
    Added README with example cases and implemented a Python solution using a set to check for duplicate elements in the input list.

---
+++ Python/217. Contains Duplicate/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if len(nums) == 0:
+            return False
+            
+        seen = set()
+
+        for num in nums:
+            if num in seen:
+                return True
+            else:
+                seen.add(num)
+
+        return False
